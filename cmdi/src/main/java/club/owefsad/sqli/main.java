e85d5e87ab51c2c628db5f9323ece0b92ac681db
owefsad <dongzhiyong@secnium.cn>
2021-12-09 15:46:40 +0800

    put multi-module to project

---
+++ cmdi/src/main/java/club/owefsad/sqli/main.java
@@ -0,0 +1,9 @@
+package club.owefsad.sqli;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("this is a cmdi package");
+    }
+
+}
---
+++ sqli/src/main/java/club/owefsad/sqli/Main.java
@@ -0,0 +1,8 @@
+package club.owefsad.sqli;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is a sqli package");
+    }
+}
