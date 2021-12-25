24671d3905a0a1fae131925a1d06168877c473d7
owefsad <dongzhiyong@secnium.cn>
2021-12-26 04:30:27 +0800

    Fix: add package config

--- src/main/java/club/owefsad/example/ExampleApplication.java
+++ src/main/java/club/owefsad/example/ExampleApplication.java
@@ -1,5 +1,6 @@
 package club.owefsad.example;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
