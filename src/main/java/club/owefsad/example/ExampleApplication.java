49c69bbf71217ee88a9361006a052694aca33718
owefsad <dongzhiyong@secnium.cn>
2022-01-09 08:25:57 +0800

    Feat: Add JNDI Injection example

--- src/main/java/club/owefsad/example/ExampleApplication.java
+++ src/main/java/club/owefsad/example/ExampleApplication.java
@@ -1,5 +1,8 @@
 package club.owefsad.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +12,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExampleApplication {
 
+    private static final Logger logger = LogManager.getLogger(ExampleApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
     }
 
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
+    }
 }
---
+++ src/main/java/club/owefsad/example/controller/JNDIInjectionController.java
@@ -0,0 +1,74 @@
+package club.owefsad.example.controller;
+
+import com.sun.jndi.url.dns.dnsURLContext;
+import com.sun.rowset.JdbcRowSetImpl;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Hashtable;
+import javax.naming.NamingException;
+import org.apache.activemq.artemis.jndi.ReadOnlyContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author owefsad
+ */
+@RestController
+@RequestMapping("/jndi")
+public class JNDIInjectionController {
+
+    private static final Logger LOGGER = (Logger) LogManager.getLogger(JNDIInjectionController.class);
+
+    /**
+     * Log4j2 JNDI Injection
+     *
+     * @param name ${jndi:ldap://localhost:2145} -> %24%7Bjndi%3Aldap%3A%2F%2Fowefsad%7D
+     * @return
+     * @throws IOException
+     */
+    @GetMapping("/log4j2-error")
+    public String runtime(String name) throws IOException {
+        LOGGER.error(name);
+        return "Log4j2 Payload is :" + name;
+    }
+
+    /**
+     * @param name ldap://localhost:2145
+     * @return
+     * @throws NamingException
+     */
+    @GetMapping("dnsURLContext")
+    public String dnsURLContext(String name) throws NamingException {
+        dnsURLContext context = new dnsURLContext(new Hashtable<>());
+        context.lookup(name);
+        return "dnsURLContext lookup for: " + name;
+    }
+
+    /**
+     * @param name ldap://localhost:2145
+     * @return
+     * @throws NamingException
+     */
+    @GetMapping("ReadOnlyContext")
+    public String ReadOnlyContext(String name) throws NamingException {
+        ReadOnlyContext context = new ReadOnlyContext();
+        context.lookup(name);
+        return "ReadOnlyContext lookup for: " + name;
+    }
+
+    /**
+     * @param name ldap://localhost:2145
+     * @return
+     * @throws SQLException
+     */
+    @GetMapping("JdbcRowSetImpl")
+    public String JdbcRowSetImpl(String name) throws SQLException {
+        JdbcRowSetImpl impl = new JdbcRowSetImpl();
+        impl.setDataSourceName(name);
+        impl.setAutoCommit(true);
+        return "JdbcRowSetImpl init for: " + name;
+    }
+}
